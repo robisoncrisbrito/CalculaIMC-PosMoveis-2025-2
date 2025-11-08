f14e7b1cbcbaefdd49bf26e676ca338e7ad8ffb2
Robison Cris Brito <44240574+robisoncrisbrito@users.noreply.github.com>
2025-11-08 08:44:05 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/utfpr/calculaimc/MainActivity.kt
@@ -0,0 +1,95 @@
+package br.edu.utfpr.calculaimc
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var etPeso: EditText
+    private lateinit var etAltura: EditText
+    private lateinit var tvResultado: TextView
+    private lateinit var btCalcular: Button
+    private lateinit var btLimpar: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        init()
+
+        btCalcular.setOnClickListener{
+            btCalcularOnClick()
+        }
+
+        btLimpar.setOnClickListener{
+            btLimparOnClick()
+        }
+
+        btLimpar.setOnLongClickListener{
+            Toast.makeText( this, "Botão limpar a tela.", Toast.LENGTH_SHORT ).show()
+            true
+        }
+    } //fim do onCreate()
+
+
+
+    private fun init(){
+        etPeso = findViewById( R.id.etPeso)
+        etAltura = findViewById(R.id.etAltura)
+        tvResultado = findViewById(R.id.tvResultado)
+        btCalcular = findViewById(R.id.btCalcular)
+        btLimpar = findViewById(R.id.btLimpar)
+    }
+
+    private fun btCalcularOnClick() {
+
+        //entrada
+        val peso = etPeso.text.toString().toDoubleOrNull()
+        val altura = etAltura.text.toString().toDoubleOrNull()
+
+        if ( peso == null ) {
+            etPeso.error = "Peso devo ser informado."
+            return
+        }
+
+        if ( altura == null ) {
+            //Toast.makeText( this, "Altura deve ser informado.", Toast.LENGTH_SHORT ).show()
+            etAltura.error = "altura devo ser informado."
+            return
+        }
+
+
+        //processamento
+        val resultado = peso / altura.pow( 2.0 )
+
+        //saída
+        tvResultado.text = "%.2f".format( resultado )
+    }
+
+    private fun btLimparOnClick() {
+        etPeso.setText( "" )
+        etAltura.setText( "" )
+        tvResultado.setText( "" )
+        etPeso.error = null
+        etAltura.error = null
+        etPeso.requestFocus()
+    }
+
+}//fim da MainActivity
+
+
